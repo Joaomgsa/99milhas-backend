b21de2a2f378c084c36028bdf315b77162dec978
Joaomgsa <joaomgsa@gmail.com>
2022-07-22 23:32:14 -0300

    Classe cliente

--- src/main/java/br/com/milhas/Cliente.java
+++ src/main/java/br/com/milhas/Cliente.java
@@ -1,7 +1,26 @@
 package br.com.milhas;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 
 public class Cliente {
-
+	@Id
+	@GeneratedValue(Strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	private String Nome;
+	private String cpf;
+	private String Email;
+	private String Telefone;
 }
