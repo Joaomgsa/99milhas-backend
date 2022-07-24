b1dd5b2abd96e9e28778cec9be89a6f83829c1e0
Joaomgsa <joaomgsa@gmail.com>
2022-07-24 17:19:41 -0300

    Escrevendo as consultas para classe cliente

--- src/main/java/br/com/milhas/Cliente.java
+++ src/main/java/br/com/milhas/Cliente.java
@@ -1,26 +1,74 @@
 package br.com.milhas;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Entity
 
 public class Cliente {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long Id;
 	
+	@Column(name = "nome")
 	private String Nome;
-	private String cpf;
+	
+	@Column(name = "cpf")
+	private String Cpf;
+	
+	@Column(name = "email")
 	private String Email;
+	
+	@Column(name = "telefone")
 	private String Telefone;
-}
+	
+	public Cliente() {
+	}
+	
+	public Cliente(String nome, String cpf, String email, String telefone) {
+		this.Nome = nome;
+		this.Cpf = cpf;
+		this.Email = email;
+		this.Telefone = telefone;
+	}
+	
+	public long getId() {
+		return Id;
+	}
+	
+	public String getNome() {
+		return Nome;
+	}
+	
+	public String getCpf() {
+		return Cpf;
+	}
+	
+	public String getEmail() {
+		return Email;
+	}
+	
+	public String getTelefone() {
+		return Telefone;
+	}
+	
+	public void setNome(String nome) {
+		this.Nome = nome;
+	}
+	
+	public void setCpf(String cpf) {
+		this.Cpf = cpf;
+	}
+	
+	public void setEmail(String email) {
+		this.Email = email;
+	}
+	public void setTelefone(String telefone) {
+		this.Telefone = telefone;
+	}
+	
+	@Override 
+	
+	public String toString() {
+		return "Cliente [id=\" + Id + \", Nome=\" + Nome + \", Cpf=\" + Cpf + \", Email=\" + Email + \"]";
+		}
+	}
